63bfdac3e86f0834a2c54f08456af0c77a1517e8
Henry Taylor <henrytaylor@live.co.uk>
2022-01-02 20:06:01 +0000

    correct letters rendering on screen

--- script.js
+++ script.js
@@ -13,29 +13,77 @@ function letterSelected(event) {
     console.log(`Chosen letter ${event.target.innerHTML}`)
     let selectedLetter = event.target.innerHTML
     checkWord(selectedLetter)
+    displayLetter(selectedLetter)
 }
 
 // variable that holds word selected from array
 let selectedWord;
+let answer;
 
 function selectWord(words) {
-    // Chooses a word from array with using random as index
     let selector = Math.floor(Math.random() * words.length)
     selectedWord = words[selector]
+    renderWord();
 }
 
-function hideSelectedButton(letter) {
-    let button = document.getElementById(`letter-${letter}`)
-    console.log(`sick ${letter.innerText}`)
-    button.style.display = "none"
+function renderWord() {
+    //actual letter
+    let letters = []
+    for (let i = 0; i < selectedWord.length; i++) {
+        letters.push(selectedWord.charAt(i));
+    }
+
+    // letterTag elements
+    let letterTags = []
+    letters.map((letter, i )=> {
+        let letterNode = document.createTextNode(letter)
+        let letterTag = document.createElement('h1')
+        letterTag.appendChild(letterNode)
+        letterTag.id = letterTag.textContent
+        letterTag.style.display = "none"
+        document.getElementById('answer-container').appendChild(letterTag)
+        letterTags.push(letterTag)
+    })
+
+    return letterTags;
 }
+
+function displayLetter(selectedLetter) {
+
+    for (let i = 0; i < selectedWord.length; i++) {
+        selectedWord.includes(selectedLetter) ? document.getElementById(selectedLetter).style.display = "block" : console.log('no')
+    }
+}
+
 
 function checkWord(letter) {
     if (selectedWord.includes(letter)) {
         console.log('Correct')
+        confirmChoice(letter)
         hideSelectedButton(letter)
-
     } else {
         console.log('Wrong letter')
     }
 }
+
+function hideSelectedButton(letter) {
+    let button = document.getElementById(`letter-${letter}`)
+    console.log(`Hide letter ${letter}`)
+    button.style.display = "none";
+}
+
+function confirmChoice (letter) {
+    // const letterNode = document.createTextNode(letter)
+    // const letterTag = document.createElement('p').appendChild(letterNode)
+    // document.getElementById('answer-container').appendChild(letterTag)
+
+    // if (selectWord().includes(letter)) {
+    //     console.log('works')
+    //
+    // }
+
+
+    // console.log('fadvsad' + selectWord(words))
+}
+
+// have selectedWord hidden from view but Make each letter appear as choice is confirmed
